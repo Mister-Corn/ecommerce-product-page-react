eb747b67fc6c9b873be070a1337c937d16e26855
Ronald Libago <me@ronaldlibago.dev>
2024-01-17 15:34:05 -0800

    Install and configure default font, Kumbh Sans

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import '@fontsource-variable/kumbh-sans'
 import './App.css'
 import mobileMenuIcon from './assets/svgs/icon-menu.svg'
 import logo from './assets/svgs/logo.svg'
@@ -5,21 +6,21 @@ import cart from './assets/svgs/icon-cart.svg'
 import avatar from './assets/image-avatar.png'
 
 function App() {
-    return (
-        <>
-            <header className="flex flex-row justify-between items-center px-6 pt-5 pb-7 bg-white">
-                <div className="flex flex-row gap-4">
-                    <img src={mobileMenuIcon} alt="Menu" />
-                    <img src={logo} alt="logo" />
-                </div>
+  return (
+    <>
+      <header className="flex flex-row items-center justify-between bg-white px-6 pb-7 pt-5">
+        <div className="flex flex-row gap-4">
+          <img src={mobileMenuIcon} alt="Menu" />
+          <img src={logo} alt="logo" />
+        </div>
 
-                <div className="flex flex-row gap-5">
-                    <img src={cart} alt="Your cart" />
-                    <img src={avatar} alt="Your account" className="w-6 h-6" />
-                </div>
-            </header>
-        </>
-    )
+        <div className="flex flex-row gap-5">
+          <img src={cart} alt="Your cart" />
+          <img src={avatar} alt="Your account" className="h-6 w-6" />
+        </div>
+      </header>
+    </>
+  )
 }
 
 export default App
