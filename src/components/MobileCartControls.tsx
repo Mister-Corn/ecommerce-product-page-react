2b98f6bec20b36662614ec640245fd4a57515f55
Ronald Libago <me@ronaldlibago.dev>
2024-01-29 12:43:20 -0800

    Integrate MobileCartControls with global state

--- src/components/MobileCartControls.tsx
+++ src/components/MobileCartControls.tsx
@@ -1,16 +1,28 @@
 import iconMinus from '@/assets/svgs/icon-minus.svg'
 import iconPlus from '@/assets/svgs/icon-plus.svg'
+import { useAppState } from '@/data/globalState'
 
 function MobileCartControls() {
+  const quantity = useAppState((state) => state.orderControls.quantity)
+  const adjustQuantity = useAppState((state) => state.adjustQuantity)
+
   return (
     <div className="mb-4 mt-6 flex w-full flex-row items-center justify-between rounded-[10px] bg-[#F6F8FD] py-5">
-      <button aria-label="Decrement quantity" className="pl-6 text-sunshine-fg">
+      <button
+        aria-label="Decrement quantity"
+        onClick={() => adjustQuantity('decrement')}
+        className="pl-6 text-sunshine-fg"
+      >
         <img src={iconMinus} alt="-" />
       </button>
       <span className="font-bold" aria-label="Current quantity to add to cart">
-        0
+        {quantity}
       </span>
-      <button aria-label="Increment quantity" className="pr-6 text-sunshine-fg">
+      <button
+        aria-label="Increment quantity"
+        onClick={() => adjustQuantity('increment')}
+        className="pr-6 text-sunshine-fg"
+      >
         <img src={iconPlus} alt="+" />
       </button>
     </div>
