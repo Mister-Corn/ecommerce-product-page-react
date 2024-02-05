a8f804d83b082cd0af88897677f62b3831ad1020
Ronald Libago <me@ronaldlibago.dev>
2024-02-05 09:29:19 -0800

    Develop desktop styling in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,23 +18,23 @@ function App() {
 
   return (
     <div className="w-full bg-white">
-      <div className="w-full md:container md:mx-auto">
+      <div className="w-full lg:container lg:mx-auto">
         {isDesktop ? <DesktopTopBar /> : <MobileTopBar />}
 
-        <main className="flex flex-col md:flex-row md:gap-16 md:p-24 lg:gap-32">
+        <main className="flex flex-col lg:flex-row lg:justify-between lg:gap-32 lg:p-24">
           <section
             id="product-display--mobile"
-            className="relative w-full md:basis-1/2"
+            className="relative w-full lg:basis-1/2"
           >
             {isDesktop ? <DesktopProductGallery /> : <MobileProductCarousel />}
           </section>
 
-          <section className="p-6 md:basis-1/2">
+          <section className="p-6 lg:basis-1/2">
             <p className="mb-5 text-xs font-bold tracking-[0.125em] text-sunshine-fg">
               SNEAKER COMPANY
             </p>
 
-            <h1 className="mb-4 text-3xl font-bold text-black-darker md:text-5xl">
+            <h1 className="mb-4 text-3xl font-bold text-black-darker lg:text-5xl">
               Fall Limited Edition Sneakers
             </h1>
 
@@ -44,7 +44,7 @@ function App() {
               everything the weather can offer.
             </p>
 
-            <div className="flex flex-row items-center justify-between md:flex-col md:items-start md:gap-2">
+            <div className="flex flex-row items-center justify-between lg:flex-col lg:items-start lg:gap-2">
               <div className="flex flex-row items-center gap-4">
                 <span className="inline-block text-[28px] font-bold text-black-darker">
                   $125.00
@@ -67,17 +67,19 @@ function App() {
               </span>
             </div>
 
-            <CartControls />
+            <div className="mt-6 grid gap-4 lg:grid-cols-12 lg:items-center">
+              <CartControls className="w-full lg:col-span-5" />
 
-            <MobileCartDialog>
-              <button
-                onClick={addFallSneakerToCart}
-                className="flex w-full flex-row items-center justify-center gap-4 rounded-[10px] bg-sunshine-fg py-5 font-bold text-white shadow-xl shadow-sunshine-fg/20"
-              >
-                <IconCart fill="white" />
-                Add to cart
-              </button>
-            </MobileCartDialog>
+              <MobileCartDialog>
+                <button
+                  onClick={addFallSneakerToCart}
+                  className="flex w-full flex-row items-center justify-center gap-4 rounded-[10px] bg-sunshine-fg py-5 font-bold text-white shadow-xl shadow-sunshine-fg/20 lg:col-span-7 lg:shadow-none"
+                >
+                  <IconCart fill="white" />
+                  Add to cart
+                </button>
+              </MobileCartDialog>
+            </div>
           </section>
         </main>
       </div>
