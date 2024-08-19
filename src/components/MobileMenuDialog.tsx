78cd6fe25a8a8bccf04457027e000931faf2baae
Ronald Libago <me@ronaldlibago.dev>
2024-08-19 16:37:55 -0700

    Change animation to slide-in and slide-out

--- src/components/MobileMenuDialog.tsx
+++ src/components/MobileMenuDialog.tsx
@@ -15,7 +15,7 @@ function MobileMenuDialog() {
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Content className="fixed left-0 top-0 z-50 h-full w-full max-w-64 bg-white p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95">
+        <Dialog.Content className="fixed left-0 top-0 z-50 h-full w-full max-w-64 bg-white p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left">
           <Dialog.DialogClose>
             <img
               src={closeIcon}
